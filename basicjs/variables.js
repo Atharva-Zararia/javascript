f2cac8aff915f0e3502a4883fbeb3d0591fc1404
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2024-12-27 05:12:25 +0000

    main

---
+++ basicjs/variables.js
@@ -0,0 +1,6 @@
+const accountId="1234";
+let accountName="Atharva";
+var accountCity="nagpur";
+accountState = "Maharashtra";
+accountState="Goa";
+console.table([accountId,accountName,accountCity,accountState]);
