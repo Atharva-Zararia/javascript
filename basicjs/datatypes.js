a9a340603f15faab681e25ccc0a08cd35e3854a0
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2024-12-29 17:33:39 +0000

    datatypes

---
+++ basicjs/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // means using latest version of js
+//alert("hello")   //we are using node not browser
+
+
+console.log(3 +
+    3
+)   // make sure to write code in such a that it is easy to ready
+
+
+console.log('Hello')  
+/* remember mdn and tc39.es*/
+//variables
+let name="atharva"//string
+let age=23 // number
+let isloggedin = true //boolean
+// number => 2 to the pwer 53
+//big int
+// string => " "
+// boolean => true/false
+//null => standalone value (representation of empty value)
+// undefined ( when value is not defined)
+//symbol => uniqueness
+
+
+
+// object datatypes
+
+console.log(typeof (age)); //
+console.log(typeof (null));
+console.log(typeof (undefined));
