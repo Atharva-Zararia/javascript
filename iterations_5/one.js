2ec59c000f6afa2f62516219e6bf322c818cbb8a
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2025-02-09 11:05:48 +0000

    loops

---
+++ iterations_5/one.js
@@ -0,0 +1,54 @@
+// FOR loop
+
+// for (let i = 0; i <=10; i++) {
+//     if(i==5)
+// {
+//     console.log(`u hit 5`);
+//     continue;
+    
+// }
+    
+//     console.log(`i value is ${i}`);
+    
+    
+// }
+
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value"${i}`);
+    
+//     for (let j = 0; j <=10; j++) {
+        
+
+//         console.log(`inner loop value : ${j} and inner loop calue : ${i}`);
+//     }
+    
+    
+// }
+
+
+
+// break and continue
+
+// for(let i=0 ; i<=20;i++)
+// {
+//     if(i==5)
+//         {
+//         console.log('5 detected');
+//         break;
+//         }
+//     console.log(`value of i is ${i}`);
+   
+// }
+for(let i=0 ; i<=20;i++)
+{
+    if(i==5)
+        {
+        console.log('5 detected');
+        continue;
+        }
+    console.log(`value of i is ${i}`);
+   
+}
+
---
+++ iterations_5/two.js
@@ -0,0 +1,23 @@
+//WHILE LOOP
+// let index = 0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+//     index++;
+// }
+// let myarray = ['flash','batman','superman']
+// let arr  = 0
+
+// while(arr < myarray.length)
+// {
+//     console.log(` heros are ${myarray[arr]}`);
+//     arr = arr+1;
+// }
+
+
+//DO WHILE loop runs atleast ...first condition is not checked in do while loop
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score ++;
+    
+}while(score <=10);
