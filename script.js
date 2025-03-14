b2faf051d51bf5f888d51f1e31e0d1b3f6613639
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2025-03-14 12:36:03 +0000

    bugs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // to get the input value from user of leaddate and store it
-let captachflag;
+let captachflag = false;
 function beforesubmit(event)
 {   
     if(captachflag)
     {
-        const outputedate = document.querySelector(".outputdate");
+    const outputedate = document.querySelector(".outputdate");
     const inputdate = document.querySelector(".inputdate"); //this date has type string convert it into date datatype (get ur locale user info run "system.debug(UserInfo.getLocale());" in developer console and search for USER_DEBUG)
    
     console.log(inputdate.value);
@@ -21,5 +21,11 @@ beforesubmit()
 // site key 6Lc4ZPQqAAAAALT4beAU3ldr-ededZBXNiNYoEWo
 // site secret key 
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "")
+         {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());
+            document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+        } 
+    } setInterval(timestamp, 500);
 
