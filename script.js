57f0aa8eba0f7cba96e1a5e98b58cd7761d8acfa
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2025-03-14 12:08:55 +0000

    main

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function beforesubmit()
      outputedate.value = formatteddate;
 }
 beforesubmit()
+
+// site key 6Lc4ZPQqAAAAALT4beAU3ldr-ededZBXNiNYoEWo
+// site secret key 
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
