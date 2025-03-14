b080191765d656b7a9aacc9d83aae9d31dd4e997
Atharva-Zararia <107107368+Atharva-Zararia@users.noreply.github.com>
2025-03-14 12:18:26 +0000

    main

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // to get the input value from user of leaddate and store it
-function beforesubmit()
-{
-    const outputedate = document.querySelector(".outputdate");
+let captachflag;
+function beforesubmit(event)
+{   
+    if(captachflag)
+    {
+        const outputedate = document.querySelector(".outputdate");
     const inputdate = document.querySelector(".inputdate"); //this date has type string convert it into date datatype (get ur locale user info run "system.debug(UserInfo.getLocale());" in developer console and search for USER_DEBUG)
    
     console.log(inputdate.value);
     let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN") //en_In is replaced by en-IN
      outputedate.value = formatteddate;
+    }
+    else{
+        alert("invalid captcha")
+        event.preventDefault();
+    }
 }
 beforesubmit()
 
@@ -14,3 +22,4 @@ beforesubmit()
 // site secret key 
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+
